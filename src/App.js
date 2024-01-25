47348ec503cfc766c42c50b2990181622ec08715
emrahkosen <emrahkosen@gmail.com>
2024-01-25 23:46:36 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Sidebar from './components/sidebar/Sidebar';
 import MainRouters from './routers/MainRouter';
 import { RouterProvider } from 'react-router-dom';
+import LeftNavbar from './components/navber/Navbar';
 
 function App() {
   return (
     <div className="app-container">
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <LeftNavbar />
       <div className="main-content">
         <RouterProvider router={MainRouters}/>
       </div>
