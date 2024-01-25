312a73e911bfd85bcdbec06b6ab561ea7ffeb851
emrahkosen <emrahkosen@gmail.com>
2024-01-26 00:02:54 +0300

    bootstrap/dist/css/bootstrap.min.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Sidebar from './components/sidebar/Sidebar';
 import MainRouters from './routers/MainRouter';
 import { RouterProvider } from 'react-router-dom';
 import LeftNavbar from './components/navber/Navbar';
@@ -7,7 +6,6 @@ import LeftNavbar from './components/navber/Navbar';
 function App() {
   return (
     <div className="app-container">
-      {/* <Sidebar /> */}
       <LeftNavbar />
       <div className="main-content">
         <RouterProvider router={MainRouters}/>
--- src/components/navber/Navbar.js
+++ src/components/navber/Navbar.js
@@ -5,56 +5,59 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function LeftNavbar() {
   return (
     <>
-        <Navbar key={false} expand={false} className="bg-body-tertiary mb-3">
-          <Container fluid>
-            <Navbar.Brand href="/">Kösen</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${false}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
-              placement="start"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${false}`}>
-                  Emrah Kösen
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="resume">Resume</Nav.Link>
-                  <Nav.Link href="about">About</Nav.Link>
-                  <NavDropdown
-                    title="Dropdown"
-                    id={`offcanvasNavbarDropdown-expand-${false}`}
-                  >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-    </>
+
+      <Navbar key={false} expand={false} className="bg-body-tertiary mb-3">
+        <Container fluid>
+          <Navbar.Brand href="/">Kösen</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${false}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+            placement="start"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${false}`}>
+                Emrah Kösen
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="resume">Resume</Nav.Link>
+                <Nav.Link href="about">About</Nav.Link>
+                <NavDropdown
+                  title="Dropdown"
+                  id={`offcanvasNavbarDropdown-expand-${false}`}
+                >
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+
+  </>
   );
 }
 
